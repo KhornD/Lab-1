b09195a5f17d24f3556b24f3792217f389f481d0
KhornD <99799855+KhornD@users.noreply.github.com>
2022-05-19 11:26:57 +0500

    Update lab5.c

--- Labs/lab5.c
+++ Labs/lab5.c
@@ -1,93 +1,30 @@
 #include <stdio.h>
-#include <time.h>
-#include <stdlib.h>
-
-void fill(int * arr, int n)
+int countOfDigits(int n)
 {
-  for (int i = 0; i < n; i++)
-    arr[i] = rand() % 101 - 50;
-}
-
-void out(int arr[], int n)
-{
-  for (int i = 0; i < n; i++)
-    printf("%4d", arr[i]);
-  printf("\n");
-}
-
-int find_count(int a[], int b[], int n1, int n2)
-{
-  int count = 0, flag = 0;
-  for (int i = 0; i < n1; i++)
-  {
-    flag = 0;
-    for (int j = 0; j < n2; j++)
+    if (!n) return 1;
+    int count_of_dg = 0;
+    while (n)
     {
-        for (int l = 0; l < i; l++)
-          if (a[i] == a[l])
-          {
-            flag = 1;
-            break;
-          }
-        if (flag) break;
-        if (a[i] == b[j])
-        {
-          count++;
-          break;
-        }
+        count_of_dg++;
+        n /= 10;
     }
-  }
-  return count;
+    return count_of_dg;
 }
 
-void find_rep(int rep[], int a[], int b[], int n1, int n2)
+int rec_countOfDigits(int n)
 {
-  int count = 0, flag = 0;
-  for (int i = 0; i < n1; i++)
-  {
-    flag = 0;
-    for (int j = 0; j < n2; j++)
-    {
-      for (int l = 0; l < i; l++)
-        if (a[i] == a[l])
-        {
-          flag = 1;
-          break;
-        }
-      if (flag) break;
-      if (a[i] == b[j])
-      {
-        rep[count++] = a[i];
-        break;
-      }
-    }
-  }
+    if (n / 10)
+        return 1 + rec_countOfDigits(n / 10);
+    else
+        return 1;
 }
 
-int main()
-{
-  int n1, n2;
-  srand(time(NULL));
-  printf("Введите длинну первого массива: ");
-  scanf("%d", &n1);
-  printf("Введите длинну второго массива: ");
-  scanf("%d", &n2);
-  int a[n1], b[n2];
-  fill(a, n1);
-  fill(b, n2);
-  printf ("Первый массив:\n");
-  out(a, n1);
-  printf ("Второй массив:\n");
-  out(b, n2);
-  int count = find_count(a, b, n1, n2);
-  if (!count)
-    printf("Нет повторяющихся элементов\n");
-  else
-  {
-    printf ("Повторяющиеся элементы:\n");
-    int rep[count];
-    find_rep(rep, a, b, n1, n2);
-    out(rep, count);
-  }
-  return 0;
+int main() {
+    int number;
+    printf("Enter integer number: ");
+    scanf("%d", &number);
+    printf("Count of digits:\n");
+    printf("Cycle: %d\n", countOfDigits(number));
+    printf("Recursion: %d\n", rec_countOfDigits(number));
+    return 0;
 }
